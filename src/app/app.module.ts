dbc35a376fc322e52c5af1a1cc37016124cf4bf4
chrisbchoi <chrisb.choi@gmail.com>
2024-12-23 16:04:28 +1100

    updated for signal updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,31 @@
 import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {createCustomElement} from '@angular/elements';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { createCustomElement } from '@angular/elements';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardComponent
-  ],
+  declarations: [AppComponent, CardComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatListModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { 
+export class AppModule {
   constructor(private injector: Injector) {
     const el = createCustomElement(AppComponent, { injector });
     customElements.define('ng-social-card', el);
   }
 
-  ngDoBootstrap(){
-    
-  }
+  ngDoBootstrap() {}
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, Input, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-card',
-    templateUrl: './card.component.html',
-    styleUrls: ['./card.component.css'],
-    standalone: false
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+  standalone: false,
 })
 export class CardComponent implements OnInit {
+  @Input() value = 0;
+  @Input() likeCount = 0;
+  @Input() shareCount = 0;
+  labelLike  = computed(() => `The slider's value is ${this.likeCount}`);
+  shareLike  = computed(() => `The slider's value is ${this.shareCount}`);
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onShare() {
+    console.log('Share fired');
+    this.shareCount += 1;
+    // throw new Error('Method not implemented.');
+  }
+  onLike() {
+    console.log('Like fired');
+    this.likeCount += 1;
+      // Create a computed expression that reads the value input
+    // this.labelLike = computed(() => `The slider's value is ${this.likeCount()}`);
+    // throw new Error('Method not implemented.');
   }
 
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 }
